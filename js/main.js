48b05ce85047d06eb3a71ffe10850f81b58d146b
MedHam94 <hammas.m.mh@gmail.com>
2023-09-21 18:07:47 +0100

    updateCell

--- js/main.js
+++ js/main.js
@@ -6,10 +6,31 @@ const cells = document.querySelectorAll('.cells')
 
 cells.forEach(cell => cell.addEventListener('click', updateCell))
 
-
+let xTurn = true
 
 function updateCell(clickedCellEven){
     const clickedCell = clickedCellEven.target
     console.log(clickedCell)
-    return clickedCell.innerHTML = 'X'
+    
+    if(xTurn && clickedCell.innerHTML === ''){
+        clickedCell.innerHTML = 'X'
+        xTurn = false
+    }else if(!xTurn && clickedCell.innerHTML === ''){
+        clickedCell.innerHTML = 'O'
+        xTurn = true
+        
+    }
+    
 }
+
+// let turn = 'X'
+// function game(id){
+//     let element = document.getElementById(id)
+//     if(turn === 'X'  && element.innerHTML == ''){
+//         element.innerHTML = 'X' <div>hello World </div>
+//         turn  = 'O'
+//     }else if(turn === 'O' && element.innerHTML == ''){
+//         element.innerHTML = 'O'
+//         turn = 'X'
+//     }
+// }
